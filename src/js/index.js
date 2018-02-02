12b99f9e02d858e2f035acfbdd413b86c180a7a8
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-02-02 10:10:26 +0900

    append emojidex js/css in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
-import $ from 'jquery';
-import emojidex from '../../node_modules/emojidex/dist/js/emojidex.js';
-import plugin from '../../../emojidex-tinymce-plugin/src/plugin.js'; // TODO:
+// TODO: emojidex のアドレス
+$('head').append('<script src="https://cdn.rawgit.com/emojidex/emojidex-web/add_option/dist/js/emojidex.js"></script>');
+$('head').append('<link href="https://cdn.emojidex.com/scripts/css/emojidex.min.css" rel="stylesheet" />');
+
+// TODO: 将来的にはnpmから
+import plugin from '../../../emojidex-tinymce-plugin/src/plugin.js';
 
 tinymce.PluginManager.add('emojidex', plugin);
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -45,5 +45,6 @@ module.exports = {
       },
       include: [/\.(js|php)$/]
     })
-  ]
+  ],
+  watch: true
 }
